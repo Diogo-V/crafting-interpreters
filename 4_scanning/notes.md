0db457c60fc774c00f68872704ca4f705cf3474b
Diogo-V <diogo.venancio00@gmail.com>
2024-01-29 18:27:30 +0000

    feat: chapter scanning

---
+++ 4_scanning/notes.md
@@ -0,0 +1,33 @@
+# Scanning
+
+Its important to handle errors in the compiler. We should always print the line, column and if possible, a visual representation of the offending line to the user.
+
+It is good practice to seperate the code that generates the errors from the code that reports them.
+
+Various phases of the frontend will detect errors bu its not their job to know how to present them to the user. Usually, we will have multiple ways of showing an error like in the IDE, in the stderr, etc...
+
+Ideally we would have an actual abstraction, some kind of "ErrorReporter" interface that gets passed to the scanner and parser so that we can swap out different reporting strategies.
+
+For Lox, since it is so simple, we didn't do that.
+
+A *Lexeme* is a set of chars that, when together, mean something to our implementation (like reserved keywords or even string variables).
+
+A Parser cares about having lexemes but more than that, it needs to know which kind of lexeme.
+
+We can have multiple types of keywords like operators and literal types.
+
+Literal values are Strings and Numbers. The Scanner has to walk each character in the literal to correctly identify it and can also convert it to the correct runtime object.
+
+Some Scanner implementations store the location as two numbers: the offset from the beginning of the source file to the beginning of the lexeme and the length of the lexeme. An offset can be converted to a line/column later by counting the number of preceding new lines. This is slow but since we only do it in an error, thats okay.
+
+"The Dragon Book" covers the theory a bit better. Should take a look at it after.
+
+The rules that dictate how a particular language bundles characters into lexemes is called *lexical grammar*.
+
+To have a better user experience, we should always surface all errors found instead of surfacing only one error at a time when scanning. This allows the user to fix all the errors in one go.
+
+The principal of *maximal munch* is when two lexical grammar rules can both match a chunk of code that the scanner is looking at, whichever one matches more characters wins. For instance, we can have an identifier called "orchid" and the keyword "or". All or statements would be incorrect if we did not handle keywords differently.
+
+To handle that case, we should capture it as an identifier and only then, can we try to compare it against all known keywords of the language.
+
+If it matches as one of the reserved keywords, we have to "promote" it to a specific token type of the reserved keyword that was matched.
---
+++ com/diogovenancio/jlox/Scanner.java
@@ -0,0 +1,208 @@
+package com.diogovenancio.jlox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.diogovenancio.jlox.TokenType.*;
+
+class Scanner {
+  private final String source;  // Stores the raw source code as a string
+  private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;  // Points to the first character in the lexeme being scanned
+  private int current = 0;  // Points to the character currently being considered
+  private int line = 1;  // Tracks which line the current lexeme is on
+
+  private static final Map<String, TokenType> keywords;  // Stores the reserved keywords
+  
+  static {  // Initializes the keywords map
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
+  Scanner (String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while(!isAtEnd()) {
+      // We are at the beginning of the next lexeme
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+  
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+
+      // Single-character tokens
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break;
+
+      // One or two character tokens
+      case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+      case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+      case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+      case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+  
+      // Comments and division. Since both share the same initial token, we need to check for the second character.
+      // If we are dealing with comments, we also need to ignore everything until the end of the line
+      case '/':
+        if (match('/')) {  // If we find a comment, we ignore everything until the end of the line
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
+
+      // Whitespace, newlines and tabs <- ignored
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        line++;
+        break;
+
+      // String literals
+      case '"':
+        string();
+        break;
+
+      // In case we find a token that is not matched by our syntax
+      default:
+        if (isDigit(c)) {  // If we find a digit, we are dealing with a number
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
+        break;
+    }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+    
+    // See if the identifier is a reserved word
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+
+    addToken(type);
+  }
+
+  private boolean isAlpha(char c) {  // Checks if the character is a letter or an underscore
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {  // Checks if the character can be considered part of an identifier
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part
+    if (peek() == '.' && isDigit(peekNext())) {  // Needs the peekNext() because we don't want to consume the "."
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private char peekNext() {  // Returns the next's next character in the source file without consuming it
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+  
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;  // Support multiline strings
+      advance();
+    }
+
+    // Unterminated string
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing "
+    advance();
+
+    // Trim the surrounding quotes
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+
+  private char peek() {  // Returns the next character in the source file without consuming it
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private boolean match(char expected) {  // Checks if the current character is the expected one
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;  // We need to increment the current pointer because we are consuming the character
+    return true;
+  }
+
+  private char advance() {  // Consumes the next character in the source file and returns it
+    current++;
+    return source.charAt(current -1);
+  }
+  
+  private void addToken(TokenType type) {  // Adds a token to the list
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {  // Adds a token to the list
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+
+}
